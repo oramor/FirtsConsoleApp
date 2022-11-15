b4d888834db19f8fd47bd2cafc5d77c69c8f449c
Roman Rodin <romaro83@gmail.com>
2022-11-15 22:35:03 +0300

    Required modifier test

--- FirtsConsoleApp/Program.cs
+++ FirtsConsoleApp/Program.cs
@@ -4,6 +4,11 @@
     {
         var s = Convert.ToString((int)SomeEnum.Two);
         Console.WriteLine(s);
+
+        var c = new ExeptionCultureNode() {
+            En_US = "dfdf",
+            Ru_RU = "sdssd"
+        };
     }
 }
 
@@ -12,4 +17,18 @@ enum SomeEnum
     One = 1,
     Two = 2,
     Three = 4
+}
+
+interface ICultureNode
+{
+    public string Ru_RU { get; init; }
+    public string En_US { get; init; }
+    public string En_CA { get; init; }
+}
+
+class ExeptionCultureNode : ICultureNode
+{
+    public string Ru_RU { get; init; }
+    public string En_US { get; init; }
+    public required string En_CA { get; init; }
 }
