1687cdb633c970f81ac7b9c8e43d8190522c744f
Roman R <romaro83@gmail.com>
2023-05-26 23:33:48 +0300

    PartialTypes proj

---
+++ PartialClasses/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        var a = new A();
+        a.PrintHello();
+        a.PrintBye();
+    }
+}
+
+partial class A : IA
+{
+    public void PrintHello() { Console.WriteLine("Hello!"); }
+}
+
+partial class A : IA
+{
+    public void PrintBye() { Console.WriteLine("Bye!"); }
+}
+
+partial interface IA
+{
+    void PrintHello();
+}
+
+partial interface IA
+{
+    void PrintBye();
+}
