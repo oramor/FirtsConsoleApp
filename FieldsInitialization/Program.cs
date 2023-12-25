689e0da9fdd6ccb79584ee9352d632cfde8bac1b
Roman Rodin <romaro83@gmail.com>
2023-12-26 00:34:56 +0300

    FieldsInitialization

---
+++ FieldsInitialization/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+var a = new A();
+//a.Print();
+
+internal class B
+{
+    int _v2;
+
+    internal B()
+    {
+        _v2 = 2;
+    }
+
+    internal B(int i)
+        : this()
+    {
+    }
+
+    internal B(string s)
+        : this()
+    {
+    }
+
+    public virtual void Print()
+    {
+
+    }
+}
+
+internal class A
+{
+    int _v1 = 1;
+
+    internal A()
+    {
+    }
+
+    internal A(int i)
+    {
+    }
+
+    internal A(string s)
+    {
+    }
+
+    //public override void Print()
+    //{
+    //    Console.WriteLine(_v1);
+    //}
+}
